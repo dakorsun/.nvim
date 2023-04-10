1bd16636efa0365dfab6d8e90f0b41b4d7e4dc3e
dkorsun <d.korsun@aimprosoft.com>
2023-04-10 19:23:45 +0300

    packer setup: ensure_packer felt better

--- lua/dakorsun/packer.lua
+++ lua/dakorsun/packer.lua
@@ -1,9 +1,12 @@
- -- Install packer
-local install_path = "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-  vim.cmd([[ packadd packer.nvim ]])
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
 local use = require("packer").use
@@ -62,7 +65,7 @@ require("packer").startup(function()
  
 	 -- Automatically set up your configuration after cloning packer.nvim
 	 -- Put this at the end after all plugins
-	 --  if packer_bootstrap then
-	 require('packer').sync()
-	 --end
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
