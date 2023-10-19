fbf2afe7bbd1983dd693b6b6ab75a84fe23d4076
Danylo Korsun <dakorsun@gmail.com>
2023-10-19 17:25:55 +0300

    dap fixes for jest config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -141,12 +141,13 @@ keymap("n", "<Leader>dr", "<CMD>lua require('dapui').float_element('repl', { ent
 -- ╰──────────────────────────────────────────────────────────╯
 require("dap-vscode-js").setup({
   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  debugger_path = os.getenv("HOME") .. "/apps/js-debug", -- Path to vscode-js-debug installation.
+  debugger_path = os.getenv("HOME") .. "/apps/js-debug",                                       -- Path to vscode-js-debug installation.
   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
   adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
   -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-  log_console_level = vim.log.levels.WARN -- Logging level for output to console. Set to false to disable console output.
+  log_console_level = vim.log.levels
+  .WARN                                   -- Logging level for output to console. Set to false to disable console output.
 })
 
 -- ╭──────────────────────────────────────────────────────────╮
@@ -210,7 +211,8 @@ for i, ext in ipairs(exts) do
       request = "launch",
       name = "Launch Test Current File (pwa-node with jest)",
       cwd = vim.fn.getcwd(),
-      runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
+      -- runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
+      runtimeArgs = { "${workspaceFolder}/node_modules/jest/bin/jest.js" },
       runtimeExecutable = "node",
       args = { "${file}", "--coverage", "false" },
       rootPath = "${workspaceFolder}",
