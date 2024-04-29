8e983a596fd3323e10dc0e9ab210bc3d5d597214
Danylo Korsun <dakorsun@gmail.com>
2024-04-29 23:35:29 +0300

    tree.nvim tweak

--- lua/dakorsun/lazy/tree.lua
+++ lua/dakorsun/lazy/tree.lua
@@ -10,8 +10,7 @@ local git_icons = {
 
 local TREE_WIDTH = 40
 
-local function on_attach(bufnr)
-    local api = require("nvim-tree").api
+local function on_attach(api, bufnr)
     local function opts(desc)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
@@ -79,8 +78,11 @@ return {
         { "<C-e>", "<cmd>lua require('nvim-tree.api').tree.toggle()<CR>", desc = "NvimTree" },
     },
     config = function()
+        local api = require('nvim-tree.api')
         require("nvim-tree").setup({
-            on_attach = on_attach,
+            on_attach = function (bufnr)
+                return on_attach(api, bufnr)
+            end,
             -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
             sync_root_with_cwd = true,
             --false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
