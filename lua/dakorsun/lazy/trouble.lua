ff3664f47ed034b2fb96b9470651f959eb5b8639
Danylo Korsun <dakorsun@gmail.com>
2024-05-31 19:51:31 +0300

    trouble v3 config updated

--- lua/dakorsun/lazy/trouble.lua
+++ lua/dakorsun/lazy/trouble.lua
@@ -1,25 +1,49 @@
 return {
     {
         "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup({
-                icons = false,
-            })
-
-            vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
-            end)
-
-            vim.keymap.set("n", "[t", function()
-                require("trouble").next({ skip_groups = true, jump = true });
-            end)
-
-            vim.keymap.set("n", "]t", function()
-                require("trouble").previous({ skip_groups = true, jump = true });
-            end)
-
-            vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<CR>")
-            vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<CR>")
-        end
+        opts = {},
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>tt",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>tT",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>ts",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>tL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>tQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+            {
+                "<leader>tn",
+                "<cmd>Trouble diagnostics next<cr>",
+                desc = "Next Diagnostics (Trouble)",
+            },
+            {
+                "<leader>tp",
+                "<cmd>Trouble diagnostics prev<cr>",
+                desc = "Previous Diagnostics (Trouble)",
+            },
+        }
     },
 }
