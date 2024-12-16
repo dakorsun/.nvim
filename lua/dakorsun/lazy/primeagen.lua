9065b3febeb77b42aa4ad8909b90b60aa019f56a
DK <dakorsun@gmail.com>
2024-12-16 14:41:42 +0200

    primeagen map added for colemak

--- lua/dakorsun/lazy/primeagen.lua
+++ lua/dakorsun/lazy/primeagen.lua
@@ -2,7 +2,7 @@ local primeagen_plugins = {
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
-        dependencies = {"nvim-lua/plenary.nvim"},
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local harpoon = require("harpoon")
 
@@ -11,23 +11,26 @@ local primeagen_plugins = {
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-u>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-i>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-o>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-p>", function() harpoon:list():replace_at(4) end)
+            -- Unleash your Colemak
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><C-e>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><C-i>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><c-o>", function() harpoon:list():replace_at(4) end)
+
+
             --Unleash your Dvorak
-            --[[ vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end) ]]
+            -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+            -- vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+            -- vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
+            -- vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
+            -- vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
         end
     },
     {
@@ -42,4 +45,3 @@ local primeagen_plugins = {
 }
 
 return primeagen_plugins
-
