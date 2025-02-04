456c29c10004fa7343c9893cae0e378c7d37c30f
DK <dakorsun@gmail.com>
2025-02-04 23:07:25 +0200

    another markdown thing

--- lua/dakorsun/lazy/markdown.lua
+++ lua/dakorsun/lazy/markdown.lua
@@ -1,8 +1,19 @@
 return {
-    'MeanderingProgrammer/markdown.nvim',
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-        require('render-markdown').setup({})
-    end,
+    -- {
+    --     'MeanderingProgrammer/markdown.nvim',
+    --     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    --     config = function()
+    --         require('render-markdown').setup({})
+    --     end,
+    -- },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 }
